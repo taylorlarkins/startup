43ada9d410d8ef9eb77e3706cbb40774bf59f6ad
taylorlarkins <tlarki7949@gmail.com>
2023-03-09 22:02:55 -0700

    made improvements to login logic

--- login.js
+++ login.js
@@ -1,26 +1,27 @@
 function login() {
-   const userNameEl = document.querySelector("#username");
-   const passEl = document.querySelector("#password");
-
-   const credentials = {
-      username: userNameEl.value,
-      password: passEl.value
-   };
+   const username = document.querySelector("#username").value;
+   const password = document.querySelector("#password").value;
    
-   if(credentials.username === "" || credentials.password === "") {
+   if(username === "" || password === "") {
       alert("You must enter a username and a password!");
-   }
-
-   let user_info = localStorage.getItem(credentials.username);
-   if(user_info === null) {
-      localStorage.setItem(credentials.username, JSON.stringify(credentials))
-      user_info = localStorage.getItem(credentials.username);
-   } 
-   user_info = JSON.parse(user_info);
-   if(user_info.password === credentials.password) {
-      localStorage.setItem("current_user", credentials.username);
-      window.location.href = "create.html";
    } else {
-      alert("Password is incorrect!");
+      let user_data = localStorage.getItem("user_data");
+      if(user_data === null) {
+         localStorage.setItem("user_data", JSON.stringify({}));
+      }
+      user_data = JSON.parse(localStorage.getItem("user_data"));
+
+      if(!(username in user_data)) {
+         user_data[username] = password;
+         localStorage.setItem("user_data", JSON.stringify(user_data))
+      }
+      correct_pass = user_data[username];
+
+      if(password === correct_pass) {
+         localStorage.setItem("current_user", username);
+         window.location.href = "create.html";
+      } else {
+         alert("Password is incorrect!");
+      }
    }
 }
